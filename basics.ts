e14b700316e5b548ec3484b0f7ef2a3c0c5fc3fc
Zidane Innis <zidaneinnis@gmail.com>
2023-02-20 11:49:06 -0500

    adding files

---
+++ basics.ts
@@ -0,0 +1,13 @@
+// Primitives: numbers, string, boolean
+//More complex types : arrays, objects
+// Function types, parameters
+
+//Primitives
+
+let age: number;
+
+age = 12;
+
+let username: string;
+
+let isIn;
