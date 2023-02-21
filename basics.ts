4deac1d436d92d9cf90aa00c6a9b50067593ae08
Zidane Innis <zidaneinnis@gmail.com>
2023-02-21 07:27:48 -0500

    Working with Array & Object Types

--- basics.ts
+++ basics.ts
@@ -10,4 +10,31 @@ age = 12;
 
 let username: string;
 
-let isIn;
+let isInstructor: boolean;
+
+isInstructor = true;
+
+// More complex tyrpes
+
+let hobbies: string[];
+
+hobbies = ["Sports", "Movies"];
+
+let person: {
+  name: string;
+  age: number;
+};
+
+person = {
+  name: "Zidane",
+  age: 12,
+};
+
+// person = {
+//   isEmployee: true,
+// };
+
+let people: {
+  name: string;
+  age: number;
+}[];
